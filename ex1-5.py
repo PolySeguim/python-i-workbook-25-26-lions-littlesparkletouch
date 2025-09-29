49304c2eb67233a8ffe08110c9d2b49f3e65f3be
littlesparkletouch <victorialouis777@gmail.com>
2025-09-29 11:29:47 -0400

    Update ex1-5.py

--- ex1-5.py
+++ ex1-5.py
@@ -6,9 +6,7 @@ address formatted in the manner that you would usually see it
 on the outside of an envelope.  Your program does not need to 
 read any input from the user.  (9 lines)
 """
-print("Victoria")
-print("8630 S Cove Dr")
-print("Cincinnati OH, 45039")
+
 """
 Exercise 2:  Hello
 Write a program that asks the user to enter his or her name.  
