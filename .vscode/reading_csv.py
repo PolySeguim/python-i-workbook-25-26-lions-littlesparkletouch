bbd7a8bb2f074f7aaa38d6d79a1b64d402f7b62e
littlesparkletouch <victorialouis777@gmail.com>
2025-11-13 11:52:06 -0500

    yay

--- .vscode/reading_csv.py
+++ .vscode/reading_csv.py
@@ -12,6 +12,9 @@ fats = []
 sodiums = []
 sugars = []
 
+list_data = []
+unique_restaurants = set()
+
 # Read the CSV file and store the data into lists
 
 def choose_file():
@@ -33,6 +36,10 @@ def read_csv(file_name):
             fats.append(float(row['fat']))
             sodiums.append(float(row['sodium']))
             sugars.append(float(row['sugars']))
+            
+            list_data.append(row)
+            unique_restaurants.add(row['restaurant'])
+            #print(unique_restaurants)
         
 def calculateAverages(list_of_values):
     average = sum(list_of_values)/len(list_of_values)
@@ -67,7 +74,7 @@ print(restaurants)
 print(calories)
 print(fats)
 
-"""
+
 #Average Calories
 average_calories = calculateAverages(calories)
 print("Average Calories:", average_calories)
@@ -83,7 +90,7 @@ print("Max Calories:", max_calories)
 #Calculate the lowest value per list
 min_calories = min_value(calories)
 print("Min Calories:", min_calories)
-""" 
+
 #Average Sugars
 average_sugars = calculateAverages(sugars)
 print("Average Sugars:", average_sugars)
@@ -100,7 +107,39 @@ print("Max Sugars:", max_sugars)
 min_sugars = min_value(sugars)
 print("Min Sugars:", min_sugars)
 
-"""
+
 #Sugar per restaurant
-print("Sugars for Burger King:", sugars_per_resaurant(sugars, "Burger King"))
+def sugars_per_restaurant(sugars, restaurant_name):
+   sugars_per = []
+   for i in range(len(restaurants)):
+        if (restaurants[i] == restaurant_name):
+            sugars_per.append(sugars[i])
+   return sum(sugars_per)
+sugars_per_restaurant(sugars, "McDonald's")
+
+def restaurants_sugar_report():
+    report = {} #dictionary to bold restaurant:sugar total pairs
+    for restaurant in unique_restaurants:
+        total_sugars = 0
+        for i in range (len(restaurants)):
+            if restaurants[i] == restaurant:
+                total_sugars += sugars[i]
+        report[restaurant] = total_sugars
+        
+    return report
+
+#Print a report of all restaurnats and the sum of theur sugars
+print("\nList of restairants and their total sugars:\n")
+report = restaurants_sugar_report()
+for result in report:
+    print(result, ":", report[result])
+print("\n")
+
+#Finds the sum of sugars per restaurant
+
+"""
+List of all items:
+[restaurant, item, type, serving size, sodium, fat]
+[0][[0],[1],[2],[3]...]
+[1][[0],[1],[2],[3]...]
 """
